f2a50327182c7de1f8ab5ddb0e180ad2a03c4d6a
filipposmertz <filipposmertz@gmail.com>
2025-06-17 20:57:12 +0300

    1st commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,17 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  standalone: false,
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
-  standalone: false,
 })
 export class AppComponent {
+  public appPages = [
+    { title: 'Αρχική', url: '/home', icon: 'home' },
+    { title: 'Λίστα Αγορών', url: '/cart', icon: 'cart' },
+    { title: 'Λίστα Επιθυμιών', url: '/wishlist', icon: 'heart' },
+    { title: 'Ιστορικό Αγορών', url: '/history', icon: 'time' }
+  ];
+
   constructor() {}
 }
