b2f60e29b6edba30fecf657cbbc0e19b03fe5c8f
filipposmertz <filipposmertz@gmail.com>
2025-06-17 21:13:32 +0300

    product details

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,10 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'product-detail',
+    loadChildren: () => import('./pages/product-detail/product-detail.module').then( m => m.ProductDetailPageModule)
+  },
 ];
 
 @NgModule({
