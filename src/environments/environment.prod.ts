e761935123ed5dd3042e735ac976f11be4446679
filipposmertz <filipposmertz@gmail.com>
2025-06-27 21:16:53 +0300

    android ready

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,16 @@
 export const environment = {
-  production: true
+  production: true,
+  
+
+  firebaseConfig: {
+    apiKey: "AIzaSyD8EiPuLj9gSi5BvxKA9CAiOi9WoDCeFzc",
+    authDomain: "super-market-app-23c22.firebaseapp.com",
+    projectId: "super-market-app-23c22",
+    storageBucket: "super-market-app-23c22.firebasestorage.app",
+    messagingSenderId: "219525253759",
+    appId: "1:219525253759:web:72c8318ce1905133b1cd4a",
+    measurementId: "G-YGL5HMLWCF"
+  }
 };
+
+
